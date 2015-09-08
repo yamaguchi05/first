9eb5166f7455892a4e5f693e0846612c57651b25
yamaguchi05 <cm220193@gmail.com>
2015-09-08 07:14:57 +0000

    バグらないように修正

--- Application/Controller/Lottery.php
+++ Application/Controller/Lottery.php
@@ -20,52 +20,94 @@ class Controller_Lottery
 		$this->objSmarty->template_dir = '/var/www/html/Application/View/' . 'Lottery';
 	}
 
+	/**
+	 * 名前順の配列を前半、後半にわけ、担当配列も10人に修正
+	 * TODO マジックナンバーを減らす
+	 */
 	public function play()
 	{
-		// 名前順 (司会者)
-		$name_orders = array('秋山','金澤','白石','袖山','豊田','丸山','宮本','村山','山口','山本');
-		// 発表者を決める順番
-		$users = array('秋山','金澤','白石','袖山','豊田','丸山','宮本','村山','山口','山本','秋山','金澤','白石','袖山','豊田','丸山','宮本','村山','山口','山本');
-		shuffle($users);
+		// 前半名前順 (司会者)
+		$first_name_orders = array('秋山','金澤','白石','袖山','豊田');
+		// 後半名前順 (司会者)
+		$last_name_orders = array('5' => '丸山'
+								 ,'6' => '宮本'
+								 ,'7' => '村山'
+								 ,'8' => '山口'
+								 ,'9' => '山本');
+		// 前半（担当者）
+		$first_users = array('秋山','金澤','白石','袖山','豊田','丸山','宮本','村山','山口','山本');
+		// 後半（担当者）
+		$last_users = array('秋山','金澤','白石','袖山','豊田','丸山','宮本','村山','山口','山本');
+		shuffle($first_users);
+		shuffle($last_users);
+
+		// 日付情報を設定
+		$start_date = '2015-09-17';
+		$first_tantou = self::tantou_array_create($first_users, $first_name_orders, $start_date);
+
+		// 2周目の日付情報を設定
+		$week_count = count($first_name_orders);
+		$second_start_date = date("Y-m-d",strtotime("+" . $week_count . "week" ,strtotime($start_date)));
+
+		$last_tantou = self::tantou_array_create($last_users, $last_name_orders, $second_start_date);
+
+		// 配列を結合
+		$tantou = $first_tantou + $last_tantou;
+
+		$this->objSmarty->assign('tantou', $tantou);
+	}
+
+	/**
+	 * 担当配列を生成する関数(名前は変更する)
+	 */
+	public function tantou_array_create($users, $name_orders, $start_date)
+	{
+		//日付情報
+		$date ='';
 		// 結果を入れる配列
 		$tantou = array();
-		//日付情報
-		$start_date = '2015-09-10';
-		$date ='';
 
-		// 担当者を取りだす
+		// 前半の担当者を取りだす
 		foreach ($name_orders as $key => $val) {
-			// 司会者とかぶらないように、発表者を出す array_shiftで引っ張るので難しい
-			// array_shiftしたあとにかぶっていたら、$usersにもっかい代入して、再度array_shiftする？
 			$tantou[$key][] = array_shift($users);
 			$tantou[$key][] = array_shift($users);
-
-			// 同じ人が2回出た場合
-			if (strcmp($tantou[$key][0], $tantou[$key][1]) === 0 ){
-				// 片方を上書き(2人出ているので再びかぶることはない)
-				$tantou[$key][1] = array_shift($users);
-				$users[] = $tantou[$key][0];
-			}
 
 			//司会者と担当者が被った場合
 			if (in_array($name_orders[$key], $tantou[$key]) === true) {
-				//司会者と担当者が被ったとのことなので、
-				//担当者2名のどっちかを判定($name_orders[$key])、array_shiftで再度ひく
-				if (strcmp($tantou[$key][0], $name_orders[$key]) === 0 ){
-					// 被った人を上書き
-					$tantou[$key][0] = array_shift($users);
+				//1回目、2回目の最後$key(4または9)の場合、
+				//前の番号のkeyの担当者と入れ替える処理を行う
+				if (strcmp($key, '4') === 0 || strcmp($key, '9') === 0 ) {
+					$pre_key = $key - 1;
+					// 前の配列をこっちにいれるために、いったんてんぷ配列に格納
+					$tmp[0] = $tantou[$pre_key][0];
+					$tmp[1] = $tantou[$pre_key][1];
+
+					$tantou[$pre_key][0] = $tantou[$key][0];
+					$tantou[$pre_key][1] = $tantou[$key][1];
+
+					$tantou[$key][0] = $tmp[0];
+					$tantou[$key][1] = $tmp[1];
 				}else{
-					$tantou[$key][1] = array_shift($users);
+					//司会者と担当者が被ったとのことなので、
+					//担当者2名のどっちかを判定($name_orders[$key])、array_shiftで再度ひく
+					if (strcmp($tantou[$key][0], $name_orders[$key]) === 0 ){
+						// 被った人を上書き
+						$tantou[$key][0] = array_shift($users);
+					}else{
+						$tantou[$key][1] = array_shift($users);
+					}
+					// 司会者と担当者が被った人を$usersにもどす
+					$users[] = $name_orders[$key];
 				}
-				// 司会者と担当者が被った人を$usersにもどす
-				$users[] = $name_orders[$key];
+
 			}
+
 
 			// 司会者情報も担当配列に代入
 			$tantou[$key][2] = $name_orders[$key];
 
-			//$keyが0（1回目）の場合は、$start_dayを代入
-			if (strcmp($key, '0') === 0) {
+			//$keyが0または5（1人目）の場合は、$start_dayを代入
+			if (strcmp($key, '0') === 0 || strcmp($key, '5') === 0 ) {
 				$tantou[$key][3] = $start_date;
 				$date = $start_date;
 			}else{
@@ -73,9 +115,8 @@ class Controller_Lottery
 				$tantou[$key][3] = date("Y-m-d",strtotime("+1 week" ,strtotime($date)));
 				$date = $tantou[$key][3];
 			}
-
 		}
-		$this->objSmarty->assign('tantou', $tantou);
+		return $tantou;
 	}
 
 	/**
